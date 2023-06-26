41b9a6297a092eda2d28adef8ee5d128877a659e
kesleymartins <kesley.martins@vivaldi.net>
2023-06-26 00:40:29 -0300

    create base schema

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_033049) do
+  create_table "projects", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.datetime "begin", null: false
+    t.datetime "end", null: false
+    t.integer "projects_id"
+    t.integer "null_id"
+    t.integer "false_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["false_id"], name: "index_works_on_false_id"
+    t.index ["null_id"], name: "index_works_on_null_id"
+    t.index ["projects_id"], name: "index_works_on_projects_id"
+  end
+
+  add_foreign_key "works", "falses"
+  add_foreign_key "works", "nulls"
+  add_foreign_key "works", "projects", column: "projects_id"
+end
