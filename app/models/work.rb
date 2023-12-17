edbda592fb9d31d5b444233e292e319df4dfcd6a
kesleymartins <kesley.martins@vivaldi.net>
2023-12-17 20:06:48 -0300

    setup work status enum with default values

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,14 @@
 class Work < ApplicationRecord
+  enum :status, { started: 0, finished: 1 }
+
   belongs_to :project
 
   scope :by_project, ->(project) { where(project:) }
+
+  before_create :defaults
+
+  def defaults
+    self.begin = DateTime.now
+    self.started = 0
+  end
 end
