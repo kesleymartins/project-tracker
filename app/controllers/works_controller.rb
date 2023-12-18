1251b5238f85041411f8c6b8c231a20c81873835
kesleymartins <kesley.martins@vivaldi.net>
2023-12-17 23:31:09 -0300

    add create/finish work actions

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,11 +1,32 @@
 class WorksController < ApplicationController
   before_action :set_project
+  before_action :set_work, only: %i[finish]
 
   def index
     @works = Work.by_project(@project)
   end
 
+  def create
+    work = @project.works.new
+
+    work.save
+
+    redirect_to @project
+  end
+
+  def finish
+    @work.finish!
+
+    @work.save
+
+    redirect_to @project
+  end
+
   private
+
+  def set_work
+    @work = Work.find(params[:id])
+  end
 
   def set_project
     @project = Project.find(params[:project_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'projects#index'
 
   resources :projects do
-    resources :works, only: %i[index]
+    resources :works, only: %i[index create] do
+      member do
+        patch 'finish'
+      end
+    end
   end
 end
